e8f9483db4fbc53950c9aa034c05adbf8353a502
Paul Oh <pauloh@Pauls-MBP.attlocal.net>
2021-06-22 20:53:19 -0500

    added authentication controller with methods, updated routes, added authorize_request to application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authorize_request, except: :create
   before_action :set_user, only: [:show, :update, :destroy]
 
   # GET /users
@@ -16,9 +17,13 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-
+    
     if @user.save
-      render json: @user, status: :created
+      @token = encode({id: @user.id})
+      render json: {
+        user: @user.attributes.except('password_digest'),
+        token: @token
+        }, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
