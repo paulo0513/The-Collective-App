9407fec2d90b8b859a5afe9609d22b13af9cce31
Paul Oh <pauloh@Pauls-MBP.attlocal.net>
2021-06-22 21:45:20 -0500

    deleted unneccessary methods from users controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get '/auth/verify', to: 'authentication#verify'
   resources :comments
   resources :nfts
-  resources :users
+  resources :users, only :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
