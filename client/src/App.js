841c2c9ae28151f45f179f3fceaa2dc1b56e9655
Paul Oh <pauloh@Pauls-MBP.attlocal.net>
2021-06-24 14:38:34 -0500

    Working thru Layout and App to test auth on front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Layout from './components/Layout/Layout'
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      
+      <Layout>
+        <Switch>
+          <Route>
+            <h3>1</h3>
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,9 +1,14 @@
 import './Layout.css'
+import { Link } from 'react-router-dom'
 
-export default function Layout() {
+export default function Layout(props) {
   return (
     <div>
-      
+      <header>
+        <h1>The Collective</h1>
+        <Link to='/login'>Login/Register</Link>
+      </header>
+      {props.children}
     </div>
   )
 }
