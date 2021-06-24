d79139adfdd16bcb641903072a155a48e334c261
Paul Oh <pauloh@Pauls-MBP.attlocal.net>
2021-06-23 23:12:28 -0500

    added psuedocode to auth.js

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,20 +1,25 @@
 import api from './api-config';
 
 export const loginUser = async (loginData) => {
-	const resp = await api.post('/auth/login', { authentication: loginData });
-	localStorage.setItem('authToken', resp.data.token);
-	api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`;
+	// directs to authentication controller
+  const resp = await api.post('/auth/login', { authentication: loginData });
+	// store token in local storage
+  localStorage.setItem('authToken', resp.data.token);
+	// attach an authorization header for each instance of axios ()
+  api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`;
 	return resp.data.user;
 };
 
 export const registerUser = async (registerData) => {
-	const resp = await api.post('/users/', { user: registerData });
+	// directs to user controller
+  const resp = await api.post('/users/', { user: registerData });
 	localStorage.setItem('authToken', resp.data.token);
 	api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`;
 	return resp.data.user;
 };
 
 export const verifyUser = async () => {
+  // get the token that was set
 	const token = localStorage.getItem('authToken');
 	if (token) {
 		api.defaults.headers.common.authorization = `Bearer ${token}`;
@@ -25,5 +30,6 @@ export const verifyUser = async () => {
 };
 
 export const removeToken = () => {
+  // logout by removing the token from local storage
 	api.defaults.headers.common.authorization = null;
 };
