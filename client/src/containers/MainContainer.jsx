b9bfb85b30cd7a7d17720a9c23ea719523d2db3b
Paul Oh <pauloh@Pauls-MBP.attlocal.net>
2021-06-25 10:46:26 -0500

    created Add Form

--- client/src/containers/MainContainer.jsx
+++ client/src/containers/MainContainer.jsx
@@ -1,14 +1,15 @@
 import "./MainContainer.css";
 import NFTs from "../screens/NFTs/NFTs";
+import EditNFT from "../screens/EditNFT/EditNFT";
+import AddNFT from "../screens/AddNFT/AddNFT";
+import DetailNFT from "../screens/DetailNFT/DetailNFT";
 import { useState, useEffect } from "react";
 import { Switch, Route, useHistory } from "react-router-dom";
 import { deleteNFT, getAllNFTs, postNFT, putNFT } from "../services/nfts";
 
-
 export default function MainContainer() {
   const [nfts, setNfts] = useState([]);
   const history = useHistory();
-  
 
   useEffect(() => {
     const fetchNfts = async () => {
@@ -17,18 +18,18 @@ export default function MainContainer() {
     };
     fetchNfts();
   }, []);
-  
+
   const handleCreate = async (formData) => {
     const nft = await postNFT(formData);
-    setFoods((prevState) => [...prevState, nft])
-    history.push('/nfts')
-  }
+    setNfts((prevState) => [...prevState, nft]);
+    history.push("/nfts");
+  };
 
   const handleUpdate = async (id, formData) => {
     const nftItem = await putNFT(id, formData);
     setNfts((prevState) =>
       prevState.map((nft) => {
-        return food.id === Number(id) ? nftItem : nft;
+        return nft.id === Number(id) ? nftItem : nft;
       })
     );
     history.push("/nfts");
@@ -37,23 +38,25 @@ export default function MainContainer() {
   const handleDelete = async (id) => {
     await deleteNFT(id);
     setNfts((prevState) => prevState.filter((nft) => nft.id !== id));
-    history.push('/nfts')
+    history.push("/nfts");
   };
 
   return (
     <div>
-      <Route path="/nfts/:id/edit">
-        <NFTs nfts={nfts} handleDelete={handleDelete} />
-      </Route>
-      <Route path="/nfts/:id">
-        <NFTs nfts={nfts} handleDelete={handleDelete} />
-      </Route>
-      <Route path="/">
-        <NFTs nfts={nfts} handleDelete={handleDelete} />
-      </Route>
-      <Route path="/">
-        <NFTs nfts={nfts} handleDelete={handleDelete} />
-      </Route>
+      <Switch>
+        <Route path="/nfts/:id/edit">
+          <EditNFT handleUpdate={handleUpdate}/>
+        </Route>
+        <Route path="/nfts/:id">
+          <DetailNFT />
+        </Route>
+        <Route path="/add-nft">
+          <AddNFT handleCreate={handleCreate} />
+        </Route>
+        <Route path="/nfts">
+          <NFTs nfts={nfts} handleDelete={handleDelete} />
+        </Route>
+      </Switch>
     </div>
   );
 }
